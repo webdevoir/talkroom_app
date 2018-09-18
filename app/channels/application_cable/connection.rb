95f7715aaca76405af7d040d08cdad38b2a6daff
EM <ememmaxcoffee@gmail.com>
2018-09-18 13:39:43 +0900

    fixed view room --> current_user right_messages, background_black

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -8,16 +8,17 @@ module ApplicationCable
 
     protected
 
-    def find_verified_user
-     if verified_user = User.find_by(id: session['user_id'])
-       verified_user
-     else
-       reject_unauthorized_connection
-     end
-    end
+      def find_verified_user
+        if verified_user = User.find_by(id: session['user_id'])
+          verified_user
+        else
+          reject_unauthorized_connection
+        end
+      end
 
-    def session
-     cookies.encrypted[Rails.application.config.session_options[:key]]
-    end
+      def session
+        cookies.encrypted[Rails.application.config.session_options[:key]]
+      end
+
   end
 end
