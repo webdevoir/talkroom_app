8341378a7d61a0631cdd4eff12ee3ce261631021
EM <ememmaxcoffee@gmail.com>
2018-10-07 13:19:30 +0900

    complete article function --> new create show index like order

--- app/assets/javascripts/article.coffee
+++ app/assets/javascripts/article.coffee
@@ -1,3 +1,5 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://coffeescript.org/
+down_button = ->
+  scrollPosition = document.getElementById('chat-scroll').scrollTop
+  scrollHeight = document.getElementById('chat-scroll').scrollHeight
+  document.getElementById('chat-scroll').scrollTop = scrollHeight
+  return
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,27 +1,50 @@
 class ArticlesController < ApplicationController
   def new
     @room = Room.find(params[:room_id])
-    @messages = @room.messages
-    @article = Article.create(title: @room.name)
-    order = 1
-    @messages.each do |message|
-      ArticleMessage.create(user_name: message.user_name, article_id: @article.id,
-       content: message.content, filename: message.filename, order: order,
-       attachment_data: message.attachment_data, created_at: message.created_at)
-       order += 1
-    end
-    @article_messages = @article.article_messages
+    @new_article_messages = @room.messages
   end
 
   def create
+    if params[:article_title] == ""
+      @article = Article.create(title: ARTICLE, like: 0)
+    else
+      @article = Article.create(title: params[:article_title], like: 0)
+    end
+  
+    order = 1
+    params[:messages].each do | message_id,judge |
+      message = Message.find(message_id)
+      if judge == "0"
+        ArticleMessage.create(user_name: message.user_name, article_id: @article.id,
+          content: message.content, filename: message.filename, order: order,
+          attachment_data: message.attachment_data, created_at: message.created_at)
+        order += 1
+      end
+    end
+    redirect_to article_path(@article.id)
   end
 
   def index
+    if params[:search] == nil || params[:search] == ''
+      @articles = Article.all.order(like: "DESC")
+      @article_heading = "ARTICLES"
+    else
+      words = params[:search].to_s.gsub(/(?:[[:space:]%_])+/, " ").split(" ")
+      query = (["title LIKE ?"] * words.size).join(" AND ")
+      @articles = Article.where(query, *words.map{|w| "%#{w}%"}).order(like: "DESC")
+      @article_heading = "SEARCH RESULT"
+    end
   end
 
   def show
+    @article = Article.find(params[:id])
+    @article_messages = @article.article_messages
   end
 
   def like
+    @article = Article.find(params[:article_id])
+    @article.like += 1
+    @article.save
+    redirect_to article_path(@article.id)
   end
 end
